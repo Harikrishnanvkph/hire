670b01210d5d86fe3b771e07298d4c152ab1d4fb
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-08-04 21:23:29 +0530

    Add files via upload

---
+++ src/JobSeeker/JSHome.jsx
@@ -0,0 +1,56 @@
+import AdbIcon from '@mui/icons-material/Adb';
+import "./JS.css"
+
+export function JSHome() {
+  return (
+    <>
+      <div className="container-fluid p-0 d-flex flex-column">
+        <nav className='row jb-nav d-flex justify-content-between p-3 m-0'>
+          <div className="d-flex justify-content-md-end justify-content-sm-center justify-content-center align-items-center">
+            <AdbIcon className="mx-1 nav-logo" fontSize="large" />
+            <h5 className="m-0">Hari HR Recruit App</h5>
+          </div>
+          <div className='profile-div'>
+            <button className='profile-div-button mx-2 mr-4'>
+              <span role="img" className='profile-div-button-span'>
+                <img className="profile-div-button-span-img" src='hari.png' alt='profile picture of Hari Krishnan V K' />
+              </span>
+            </button>
+          </div>
+        </nav>
+        <div className='row jb-body m-0 p-0'>
+          <div className='col-3 jb-menu'>
+            <div>s</div>
+          </div>
+          <div className='col-9 jb-section'>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
